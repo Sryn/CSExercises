5c2dfd23a67e376141fcafa511c93d7c105afa6d
Sryn <e0001302@u.nus.edu>
2015-08-14 16:38:20 +0800

    e0001302 Ex04

--- CSExercises/Ex01-06/Ex04.cs
+++ CSExercises/Ex01-06/Ex04.cs
@@ -10,7 +10,9 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Ex04 - Please type a double: ");
+            double aDouble = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The squareroot of the double {0} is {1}", aDouble, Math.Sqrt(aDouble));
         }
     }
 }
