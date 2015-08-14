7424e6b2ec94974b433421cdaf3f5bb4177e1c88
Sryn <e0001302@u.nus.edu>
2015-08-14 16:32:32 +0800

    e0001302 Ex03

--- CSExercises/Ex01-06/Ex03.cs
+++ CSExercises/Ex01-06/Ex03.cs
@@ -10,7 +10,9 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Please type a double: ");
+            double aString = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The square of the double {0} is {1}", aString, aString * aString);
         }
     }
 }
