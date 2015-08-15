d4ed83ed605bee5ac70cfad56c43d770edf7c28b
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-15 16:39:41 +0800

    e0001302 Ex14

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,29 @@
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Ex14 - Please type the lengths of the three sides of a Triangle and we'll calculate its area using Heron's Formula\nSide A: ");
+            double dblSideA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Side B: ");
+            double dblSideB = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Side C: ");
+            double dblSideC = Convert.ToDouble(Console.ReadLine());
+            double dblHalfPerimeter = (dblSideA + dblSideB + dblSideC) / 2;
+            double Area = Math.Sqrt(dblHalfPerimeter * (dblHalfPerimeter - dblSideA) * (dblHalfPerimeter - dblSideB) * (dblHalfPerimeter - dblSideC));
+            // This formula here will output NaN if the three given sides does not make a 'valid' Triangle
+            // An easy way to check whether the given sides makes a valid triangle is to make sure that dblHalfPerimeter is at least of equal length to each of the sides
+            if ((dblHalfPerimeter < dblSideA) || (dblHalfPerimeter < dblSideB) || (dblHalfPerimeter < dblSideC))
+                Console.WriteLine("Given sides do not make a valid triangle");
+            else
+                Console.WriteLine(Area);
         }
     }
 }
