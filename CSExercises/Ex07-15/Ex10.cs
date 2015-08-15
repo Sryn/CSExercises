ca215d3fd5cd7033e357698193f725eb6c72da17
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-15 15:15:00 +0800

    e0001302 Ex10

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,7 +10,15 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Ex10 - Please type the (x,y) coordinates of two points and we'll calculate the distance between those two coordinates\nx1: ");
+            double x1dbl = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y1: ");
+            double y1dbl = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x2: ");
+            double x2dbl = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y2: ");
+            double y2dbl = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0}", Math.Sqrt(((x2dbl - x1dbl) * (x2dbl - x1dbl)) + ((y2dbl - y1dbl) * (y2dbl - y1dbl))));
         }
     }
 }
