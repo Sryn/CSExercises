89ea53a896a72aac7c3097934a690f7d1ca281cd
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-15 14:53:13 +0800

    e0001302 Ex09

--- CSExercises/Ex07-15/Ex09.cs
+++ CSExercises/Ex07-15/Ex09.cs
@@ -10,7 +10,9 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Ex09 - Please type a value for x and we'll calculate y=2(x^2)-4x+3:");
+            double aDoubleX = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0:0.#}", (2 * aDoubleX * aDoubleX) - (4 * aDoubleX) + 3);
         }
     }
 }
