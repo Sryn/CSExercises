95d04a567fd6022567c28c7c84f42ab5cda08bbf
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-15 10:35:31 +0800

    e0001302 Ex07

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -10,7 +10,9 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Ex07 - Please type your salary and we'll show you the total of your salary + 10% housing and 3% transport allowances:");
+            double Salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("${0:0.00}", Salary + (Salary * 0.1) + (Salary * 0.03));
         }
     }
 }
