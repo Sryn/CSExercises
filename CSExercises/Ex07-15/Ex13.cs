67e86e47983f8e91c86344be0ff8c5063720e23c
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-15 15:51:48 +0800

    e0001302 Ex13

--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -10,7 +10,10 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Ex11 - Please type a taxi distance travelled in km and we'll calculate the fare for you using the formula $2.40 + $0.40 * Distance in km, rounded-up to the nearest $0.10: ");
+            double dblDistance = Convert.ToDouble(Console.ReadLine());
+            double dblFare = Math.Ceiling(10 * (2.4 + (0.4 * dblDistance))); // ceiling multiply by 10 to round up to the nearest 0.10
+            Console.WriteLine("{0:0.00}", dblFare/10); // divide by 10 to move the decimal point back 1 step to left                       
         }
     }
 }
