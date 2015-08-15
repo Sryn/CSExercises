efcd79573658fbce69342e51f1d83ed0954b29f9
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-15 17:12:48 +0800

    e0001302 Ex15

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,26 @@
+using System;
+
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Ex15 - Please type a three digit integer within the range 100-999 and we'll determine whether it is an Armstrong number or not: ");
+            int aNumber = Convert.ToInt32(Console.ReadLine());
+            if ((aNumber < 100) || (aNumber > 999))
+                Console.WriteLine("Number entered not accepted");
+            else
+            {
+                double firstDigit, secondDigit, thirdDigit;
+                firstDigit = aNumber / 100;
+                secondDigit = (aNumber / 10) - (firstDigit * 10);
+                thirdDigit = aNumber - (firstDigit * 100) - (secondDigit * 10);
+                if (aNumber == Math.Pow(firstDigit, 3) + Math.Pow(secondDigit, 3) + Math.Pow(thirdDigit, 3))
+                    Console.WriteLine("Armstrong Number");
+                else
+                    Console.WriteLine("Not Armstrong Number");
+            }
         }
     }
 }
