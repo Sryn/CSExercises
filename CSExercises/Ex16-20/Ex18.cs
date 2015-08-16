4da585c786a380834eeef2b0b78178a8940d7622
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-16 13:53:54 +0800

    e0001302 Ex18

--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -1,10 +1,23 @@
+using System;
+
 namespace CSExercises
 {
     public class Ex18
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Ex18 - Please type in your mark (0-100) and we'll show you your grade: ");
+            int mark = Convert.ToInt32(Console.ReadLine());
+            if ((mark < 0) || (mark > 100))
+                Console.WriteLine("**Error**");
+            else if (mark < 40)
+                Console.WriteLine("You scored {0} marks which is F grade.", mark);
+            else if (mark < 60)
+                Console.WriteLine("You scored {0} marks which is C grade.", mark);
+            else if (mark < 80)
+                Console.WriteLine("You scored {0} marks which is B grade.", mark);
+            else
+                Console.WriteLine("You scored {0} marks which is A grade.", mark);
         }
     }
 }
