053f6a1e0395216b4c2b7cf6158993a4ef376a28
H 'e0001302' + 'A0137786M' Masri <aiskrim+github.c0m@gmail.com>
2015-08-16 13:29:32 +0800

    e0001302 Ex16

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -1,10 +1,21 @@
+using System;
+
 namespace CSExercises
 {
     public class Ex16
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Ex16 - Please type your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please type your gender (M/F): ");
+            char gender = Convert.ToChar(Console.ReadLine());
+            if(gender == 'M')
+                Console.WriteLine("Good Morning Mr. {0}", name);
+            else if(gender == 'F')
+                Console.WriteLine("Good Morning Ms. {0}", name);
+            else
+                Console.WriteLine("Undefined gender {0}", gender);
         }
     }
 }
